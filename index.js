af909a2255587299f78cbe3e76549da7081eecd9
subrat <subratj611@gmail.com>
2023-05-26 09:35:16 +0530

    refactor: DB local connection add

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 
+const DBconnect = require("./config/mongodb");
+
 const PORT = 8000;
 
 const app = express();
 
 // connections
-
+DBconnect().then(() => console.log(`Database Connected`));
 
 // middlewares
 app.use(cors());
-
 
 // routes
 
