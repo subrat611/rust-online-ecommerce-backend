e396b4fb5ce827bac8786fd4f9f9933da56279fe
subrat <subratj611@gmail.com>
2023-05-26 18:14:10 +0530

    fix:mongodb atlas connection

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,6 +1,5 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // async function DBconnect() {
 //   return mongoose.connect("mongodb://localhost:27017/rust-store");
@@ -8,16 +7,11 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const uri = process.env.ATLAS_PASSWD;
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
 async function DBconnect() {
-  return await client.connect();
+  return await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 }
 
 module.exports = DBconnect;
