a411644a6cbcb65f8efa3aa238ade1737ee147a9
subrat <subratj611@gmail.com>
2023-05-28 22:52:22 +0530

    atlas db select

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-async function DBconnect() {
-  return mongoose.connect("mongodb://localhost:27017/rust-store");
-}
-
-// const uri = process.env.ATLAS_PASSWD;
-
 // async function DBconnect() {
-//   return await mongoose.connect(uri, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   });
+//   return mongoose.connect("mongodb://localhost:27017/rust-store");
 // }
+
+const uri = process.env.ATLAS_PASSWD;
+
+async function DBconnect() {
+  return await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+}
 
 module.exports = DBconnect;
